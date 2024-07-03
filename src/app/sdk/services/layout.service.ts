3d999f58a8f3d09c9eee3c36802eb7159b44c38f
Martin <martin.gebov@progress.com>
2024-07-03 11:03:00 +0300

    patch layout call

--- src/app/sdk/services/layout.service.ts
+++ src/app/sdk/services/layout.service.ts
@@ -7,12 +7,35 @@ import { RootUrlService } from "src/app/services/root-url.service";
 
 @Injectable()
 export class LayoutService {
+    private readonly systemQueryParams = ["sfaction", "sf_version", "segment", "sf_site", "sf_site_temp", "sf-auth", "abTestVariationKey", "sf-content-action", "sf-lc-status"];
     constructor(private http: HttpClient, private rootUrlService: RootUrlService) {
 
     }
 
     public get(pagePathAndQuery: string): Observable<PageLayoutServiceResponse> {
-        let serviceUrl = `${this.rootUrlService.getServiceUrl()}/pages/Default.Model(url=@param)?@param='${encodeURIComponent(pagePathAndQuery)}'`;
+        let systemQuery: string = '';
+
+        if (pagePathAndQuery.indexOf("?") !== -1) {
+            const index = pagePathAndQuery.indexOf("?");
+
+            const query = pagePathAndQuery.substring(pagePathAndQuery.indexOf("?"));
+            const parsedParams = new URLSearchParams(query);
+            
+            const systemQueryList: Array<{key: string, val: string}> = [];
+            const customQueryList: Array<{key: string, val: string}> = [];
+            parsedParams.forEach((paramVal, paramKey) => {
+                if (this.systemQueryParams.some(x => x === paramKey)) {
+                    systemQueryList.push({ key: paramKey, val: paramVal });
+                } else {
+                    customQueryList.push({ key: paramKey, val: paramVal });
+                }
+            });
+            
+            pagePathAndQuery = pagePathAndQuery.substring(0, index) + "?" + customQueryList.map(x => `${x.key}=${x.val}`).join('&');
+            systemQuery =  "&" + systemQueryList.map(x => `${x.key}=${x.val}`).join('&');
+        }
+
+        let serviceUrl = `${this.rootUrlService.getServiceUrl()}pages/Default.Model(url=@param)?@param='${encodeURIComponent(pagePathAndQuery)}'${systemQuery}`;
         return this.http.get<PageLayoutServiceResponse>(serviceUrl);
     }
 
